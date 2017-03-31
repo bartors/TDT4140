2785c352f36aeb4470601e4553983d56acbc7504
martoem <haugers92@hotmail.com>
2017-03-31 10:35:49 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,27 +1,11 @@
-# [Start Bootstrap](http://startbootstrap.com/) - [Freelancer](http://startbootstrap.com/template-overviews/freelancer/)
+1:DATABASE:
 
-[Freelancer](http://startbootstrap.com/template-overviews/freelancer/) is a one page freelancer portfolio theme for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/). This theme features several content sections, a responsive portfolio grid with hover effects, full page portfolio item modals, and a working PHP contact form.
+If you vant to set up just the web service and connect to our designated database, it is a posibility, however you'll have to connet via an NTNU connection or use a VPN. It is aswell not known how long the service will run on NTNUs servers. Therfore it is higly recommended that you use the SQLcode in DB/DBcode.txt to create your own ClassMateDB.
 
-## Getting Started
+It occurs there is a problem with creating unique key on MariaDB because of the length. If it occurs, change the length of string to 100 charackers (from varchar(255) to varchar(100)), but remember to change the if statement that check if the string is of valid length in register.php.
 
-To begin using this template, choose one of the following options to get started:
-* [Download the latest release on Start Bootstrap](http://startbootstrap.com/template-overviews/freelancer/)
-* Clone the repo: `git clone https://github.com/BlackrockDigital/startbootstrap-freelancer.git`
-* Fork the repo
 
-## Bugs and Issues
+2:SECURITY:
+Our security system uses a public salt that is stored in PHP, and two random generated private salts which are stored in database. We use aswell a username as forth salt. Given that the project is open source and the method of hasing pasword, and connecting the various parts of a password are open to public and easy accesible any user that wants to deploy its own ClassMate service should change the public salt and change the way a password is made.  
 
-Have a bug or an issue with this template? [Open a new issue](https://github.com/BlackrockDigital/startbootstrap-freelancer/issues) here on GitHub or leave a comment on the [template overview page at Start Bootstrap](http://startbootstrap.com/template-overviews/freelancer/).
-
-## Creator
-
-Start Bootstrap was created by and is maintained by **[David Miller](http://davidmiller.io/)**, Owner of [Blackrock Digital](http://blackrockdigital.io/).
-
-* https://twitter.com/davidmillerskt
-* https://github.com/davidtmiller
-
-Start Bootstrap is based on the [Bootstrap](http://getbootstrap.com/) framework created by [Mark Otto](https://twitter.com/mdo) and [Jacob Thorton](https://twitter.com/fat).
-
-## Copyright and License
-
-Copyright 2013-2016 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/gh-pages/LICENSE) license.
+We will aswell recommend obtaining a SSL certificate and using a secure connection (HTTPS).
--- register.php
+++ register.php
@@ -1,33 +1,31 @@
 <?php
-require ('connect.php');
+require 'connect.php';
+require 'lib/security.php';
 // If the values are posted, insert them into the database.
-//registrerer brukeren
-function registerUser($connection,$username,$email,$password,$teacher){
-	//trenger en logikk som skjekker om variablene ikke overskirder en lengde på 255
+// registrerer brukeren
+function registerUser($connection, $username, $email, $password, $teacher,$salt1,$salt2) {
+	// trenger en logikk som skjekker om variablene ikke overskirder en lengde på 255
 	if ($teacher) {
-		$query = "INSERT INTO `users` (username, password, email ,active, role) VALUES ('$username', '$password', '$email', 1,'T')";
+		$query = "INSERT INTO `users` (username, password, email , role,salt1,salt2) VALUES ('$username', '$password', '$email','T','$salt1','$salt2')";
 	} else {
-		$query = "INSERT INTO `users` (username, password, email ,active, role) VALUES ('$username', '$password', '$email', 1,'S')";
+		$query = "INSERT INTO `users` (username, password, email , role,salt1,salt2) VALUES ('$username', '$password', '$email','S','$salt1','$salt2')";
 	}
 	return mysqli_query ( $connection, $query );
 }
 
 if (isset ( $_POST ['username'] ) && isset ( $_POST ['password'] )) {
-	/*$username = $_POST ['username'];
-	$email = $_POST ['email'];
-	$password = $_POST ['password'];
-	
-	if ($_POST ['teacher']) {
-		$query = "INSERT INTO `users` (username, password, email ,active, role) VALUES ('$username', '$password', '$email', 1,'T')";
+	if (strlen ( $_POST ['username'] ) < 256 && strlen ( $_POST ['email'] ) < 256 & strlen ( $_POST ['password'] ) < 256) {
+		$salt1=getRandomString();
+		$salt2=getRandomString();
+		$password=createPassword($publicSalt, $_POST ['username'], $salt1, $salt2, $_POST ['password']);
+		$result = registerUser ( $connection, $_POST ['username'], $_POST ['email'], $password, $_POST ['teacher'],$salt1,$salt2 );
+		if ($result) {
+			$smsg = "User Created Successfully.";
+		} else {
+			$fmsg = "User Registration Failed";
+		}
 	} else {
-		$query = "INSERT INTO `users` (username, password, email ,active, role) VALUES ('$username', '$password', '$email', 1,'S')";
-	}
-	$result = mysqli_query ( $connection, $query );*/
-	$result=registerUser($connection, $_POST['username'], $_POST['email'], $_POST['password'], $_POST['teacher']);
-	if ($result) {
-		$smsg = "User Created Successfully.";
-	} else {
-		$fmsg = "User Registration Failed";
+		$fmsg = "Your email, username and password cannot have more than 255 characters.";
 	}
 	unset ( $_POST ['username'] );
 	unset ( $_POST ['email'] );
@@ -47,7 +45,7 @@ if (isset ( $_POST ['username'] ) && isset ( $_POST ['password'] )) {
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="index.css">
+<link rel="stylesheet" href="css/index.css">
 
 <!-- Latest compiled and minified JavaScript -->
 <script
@@ -64,14 +62,16 @@ if (isset ( $_POST ['username'] ) && isset ( $_POST ['password'] )) {
 				<form class="inputs" method="POST">
         <?php if(isset($fmsg)){ ?><div class="alert alert-danger"
 						role="alert"> <?php echo $fmsg; ?> </div><?php }?>
-		<?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"><?php echo $smsg; ?> </div><?php }?>
+		<?php if(isset($smsg)){ ?><div class="alert alert-success"
+						role="alert"><?php echo $smsg; ?> </div><?php }?>
         <input type="text" name="username" placeholder="Username"
 						class="form-control" required> <input type="email" name="email"
 						placeholder="E-Mail Address" class="form-control" required> <input
 						type="password" name="password" placeholder="Password"
 						class="form-control" required>
 					<div class="buttonHolder">
-						<input type="checkbox" name="teacher" value="teacher"> I am a Teacher
+						<input type="checkbox" name="teacher" value="teacher"> I am a
+						Teacher
 						<button class="submitBtn" type="Submit" id="submitBtn"
 							name="submitBtn">Register</button>
 					</div>
